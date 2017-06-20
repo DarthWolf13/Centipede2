fa12262ab849dee2841571dcccfafc2dc819afc4
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-06-20 20:31:46 +0200

    Step 2

--- Centipede2/Player.cs
+++ Centipede2/Player.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,30 @@ namespace Centipede2
     class Player : SpriteGameObject
     {
         Vector2 startPos;
+        int speed = 200;
 
         public Player() : base("spr_player")
         {
             startPos = new Vector2(235, 500);
             this.position = startPos;
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            base.HandleInput(inputHelper);
+
+            if (inputHelper.IsKeyDown(Keys.Left) && this.position.X > 0)
+            {
+                this.velocity = new Vector2(-speed, 0);
+            }
+            else if (inputHelper.IsKeyDown(Keys.Right) && this.position.X + this.Width < Centipede.Screen.X)
+            {
+                this.velocity = new Vector2(speed, 0);
+            }
+            else
+            {
+                this.velocity = Vector2.Zero;
+            }
         }
     }
 }
