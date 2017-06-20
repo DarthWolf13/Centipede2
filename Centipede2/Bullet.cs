da96f55a737800a2805569fca3bf76bdfd210e79
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-06-20 21:16:04 +0200

    Step 3

---
+++ Centipede2/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Centipede2
+{
+    class Bullet : SpriteGameObject
+    {
+        Vector2 speed;
+        Vector2 startPos;
+
+        public Bullet() : base("spr_bullet")
+        {
+            startPos = new Vector2(-200, -200);
+
+            this.Reset();
+        }
+
+        public override void Reset()
+        {
+            base.Reset();
+
+            speed = Vector2.Zero;
+            this.position = startPos;
+        }
+
+        public void Fire(Vector2 position)
+        {
+            this.position = position;
+            speed = new Vector2(0, -200);
+            this.velocity = speed;
+        }
+    }
+}
--- Centipede2/PlayingState.cs
+++ Centipede2/PlayingState.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +10,26 @@ namespace Centipede2
     class PlayingState : GameObjectList
     {
         Player player;
+        Bullet bullet;
 
         public PlayingState()
         {
             player = new Player();
+            bullet = new Bullet();
 
             this.Add(new SpriteGameObject("spr_background"));
             this.Add(player);
+            this.Add(bullet);
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            base.HandleInput(inputHelper);
+
+            if (inputHelper.KeyPressed(Keys.Space))
+            {
+                bullet.Fire(new Vector2(player.Position.X + player.Width/2 - bullet.Width/2, player.Position.Y + 20));
+            }
         }
     }
 }
