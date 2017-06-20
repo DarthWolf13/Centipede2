c250b6d18a654b0f8ba5d2d4fcb2b4eef621dc95
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-06-20 21:35:25 +0200

    Step 4

--- Centipede2/PlayingState.cs
+++ Centipede2/PlayingState.cs
@@ -11,15 +11,31 @@ namespace Centipede2
     {
         Player player;
         Bullet bullet;
+        GameObjectList snakeSegments;
+        int snakeLenght = 10;
 
         public PlayingState()
         {
             player = new Player();
             bullet = new Bullet();
+            snakeSegments = new GameObjectList();
 
             this.Add(new SpriteGameObject("spr_background"));
             this.Add(player);
             this.Add(bullet);
+            this.Add(snakeSegments);
+
+            for(int i = 0; i < snakeLenght; i++)
+            {
+                if(i < snakeLenght - 1)
+                {
+                    this.snakeSegments.Add(new SnakeSegment(i * 32 + 1, 0, "spr_snakebody"));
+                }
+                if(i == snakeLenght - 1)
+                {
+                    this.snakeSegments.Add(new SnakeSegment(i * 32, 0, "spr_snakehead"));
+                }
+            }
         }
 
         public override void HandleInput(InputHelper inputHelper)
---
+++ Centipede2/SnakeSegment.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Centipede2
+{
+    class SnakeSegment : SpriteGameObject
+    {
+        Vector2 startPos;
+        int speed = 200;
+
+        public SnakeSegment(int x, int y, string assetname) : base(assetname) 
+        {
+            startPos = new Vector2(x, y);
+            this.position = startPos;
+            this.velocity = new Vector2(speed, 0);
+        }
+
+        public void Bounce()
+        {
+            this.position.Y += 32;
+            this.velocity *= -1;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            if(this.position.X <= 0 || this.position.X + this.Width >= Centipede.Screen.X)
+            {
+                Bounce();
+            }
+        }
+    }
+}
